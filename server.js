523cc7f3b1ef30b879eb876d74aa80ea98f3025b
Birgir Palsson <birgir.palsson@gmail.com>
2019-04-08 10:13:06 +0000

    link test

--- server.js
+++ server.js
@@ -65,7 +65,8 @@ app.use(expressValidator({
     }
 }));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/client/build/index.html")));
+//app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/client/build/index.html")));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname + "/client/build/index.html")));
 
 // Start server!
 app.listen(port, () => {
