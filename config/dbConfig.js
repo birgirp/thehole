bd1a3e8ef7c0aea6db67da691f70e7fa93be437a
Birgir Palsson <birgir.palsson@gmail.com>
2019-05-25 14:03:16 +0000

    db config

---
+++ config/dbConfig.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    "dbType": "pgHeroku",
+        "dbconnection": {
+            "host": "ec2-23-23-195-205.compute-1.amazonaws.com",
+            "database": "d9nps47np7nuj2",
+            "user": "wdjicccgirusxg",
+            "port": 5432,
+            "password": "80f4480939e4da993143ffb126e48a15a5555f40fb595a4e5ae4c9941e727908",
+            "ssl": true
+
+    }
+};
