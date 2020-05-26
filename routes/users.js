ff1e1be2ec8e81d6c66dfa4abd5625bbc89a53d2
Birgir <birgir.palsson@gmail.com>
2020-05-26 13:12:24 +0000

    gmail changed passw

--- routes/users.js
+++ routes/users.js
@@ -220,7 +220,7 @@ router.post('/sendresetemail', async (req, res) => {
         text:
           'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n' +
-          `http://localhost:3066/reset/${token}\n\n` +
+          `https://thehole.herokuapp.com/reset/${token}\n\n` +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n',
       }
 
