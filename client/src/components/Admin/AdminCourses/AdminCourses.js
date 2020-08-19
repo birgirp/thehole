1954bc143621e7796b868edb0a36061d589a6d2c
Birgir <birgir.palsson@gmail.com>
2020-08-19 19:38:02 +0000

    minor

--- client/src/components/Admin/AdminCourses/AdminCourses.js
+++ client/src/components/Admin/AdminCourses/AdminCourses.js
@@ -1,136 +1,174 @@
 // External libs
-import React, { Component } from "react";
-import { Table, Button, Modal, Icon } from "semantic-ui-react";
-import axios from "axios";
+import React, { Component } from 'react'
+import { Table, Button, Modal, Icon } from 'semantic-ui-react'
+import axios from 'axios'
 
 // Own components
-import Loading from "../../Loading/Loading";
-import EditCourse from "./EditCourse";
-import CreateCourse from "./CreateCourse";
+import Loading from '../../Loading/Loading'
+import EditCourse from './EditCourse'
+import CreateCourse from './CreateCourse'
 
 class AdminCourses extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       courses: [],
       addingcourse: false,
       addingHoles: false,
-      courseId: "",
-      courseName: ""
+      courseId: '',
+      courseName: '',
     }
   }
 
-
   componentDidMount() {
-    axios.get("/api/getallcourses")
-      .then(res => {
-      
-        if(res.length===0){
-          console.log("No courses found")
-        }else {
-        this.setState({ courses: res.data })
-       
+    axios
+      .get('/api/getallcourses')
+      .then((res) => {
+        if (res.length === 0) {
+          console.log('No courses found')
+        } else {
+          this.setState({ courses: res.data })
         }
-
       })
-      .catch(err => {
-        console.log(err);
+      .catch((err) => {
+        console.log(err)
       })
   }
 
   addNewCourse = (course) => {
     let courses = null
     if (!this.state.courses) {
-      courses = [];
+      courses = []
     } else {
-      courses = this.state.courses;
+      courses = this.state.courses
     }
-    courses.push(course);
-    this.setState({ courses: courses });
+    courses.push(course)
+    this.setState({ courses: courses })
   }
 
   closeCreateModal = () => {
-    this.setState({ addingcourse: false });
+    this.setState({ addingcourse: false })
   }
 
   closeHolesModal = () => {
-    this.setState({ addingHoles: false });
+    this.setState({ addingHoles: false })
   }
 
   handleAddCourse = () => {
-    if (window.location.pathname !== "/admin/createcourse") {
-      console.log("adding course...")
+    if (window.location.pathname !== '/admin/createcourse') {
+      console.log('adding course...')
       this.setState({ addingcourse: true })
     }
   }
   editCourse = (courseId, courseName) => {
-    this.setState({ courseId: courseId, courseName: courseName, addingHoles: true })
+    this.setState({
+      courseId: courseId,
+      courseName: courseName,
+      addingHoles: true,
+    })
   }
-
 
   render() {
     if (!this.state.courses) {
       return (
         <div>
           <span>No courses available</span>
-          <br /><br />
-          <Button primary onClick={this.handleAddCourse}>Add new Course</Button>
-          <Modal size="fullscreen" open={this.state.addingcourse} onClose={this.closeCreateModal}>
+          <br />
+          <br />
+          <Button primary onClick={this.handleAddCourse}>
+            Add new Course
+          </Button>
+          <Modal
+            size='fullscreen'
+            open={this.state.addingcourse}
+            onClose={this.closeCreateModal}
+          >
             <Modal.Header>Add new course</Modal.Header>
-            <Modal.Content >
-              {<CreateCourse closeModal={this.closeCreateModal} addNewCourse={this.addNewCourse} />}
+            <Modal.Content>
+              {
+                <CreateCourse
+                  closeModal={this.closeCreateModal}
+                  addNewCourse={this.addNewCourse}
+                />
+              }
             </Modal.Content>
           </Modal>
         </div>
       )
     }
     if (this.state.courses.length === 0) {
-      return (
-        <Loading />
-      )
+      return <Loading />
     } else {
-      const data = this.state.courses;
+      const data = this.state.courses
       console.log(data)
       return (
         <div>
-          <Button primary onClick={this.handleAddCourse}>Add new Course</Button>
+          <Button primary onClick={this.handleAddCourse}>
+            Add new Course
+          </Button>
 
-          <br /><br />
+          <br />
+          <br />
           <Table celled>
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell width='1'>Edit</Table.HeaderCell>
                 <Table.HeaderCell width='3'>Name</Table.HeaderCell>
                 <Table.HeaderCell width='2'>Tee</Table.HeaderCell>
-                <Table.HeaderCell width='3'>Country</Table.HeaderCell>
-
+                <Table.HeaderCell width='3'>Location</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
               {data.map((course) => {
                 return (
                   <Table.Row key={course.id}>
-                    <Table.Cell ><Icon name='edit' link onClick={() => this.editCourse(course.id, course.course_name)} ></Icon></Table.Cell>
-                    <Table.Cell >{course.course_name}</Table.Cell>
-                    <Table.Cell >{course.tee}</Table.Cell>
-                    <Table.Cell >{course.country}</Table.Cell>
+                    <Table.Cell>
+                      <Icon
+                        name='edit'
+                        link
+                        onClick={() =>
+                          this.editCourse(course.id, course.course_name)
+                        }
+                      />
+                    </Table.Cell>
+                    <Table.Cell>{course.course_name}</Table.Cell>
+                    <Table.Cell>{course.tee}</Table.Cell>
+                    <Table.Cell>{course.country}</Table.Cell>
                   </Table.Row>
-                );
+                )
               })}
             </Table.Body>
           </Table>
-          <Modal size="fullscreen" open={this.state.addingcourse} onClose={this.closeCreateModal}>
+          <Modal
+            size='fullscreen'
+            open={this.state.addingcourse}
+            onClose={this.closeCreateModal}
+          >
             <Modal.Header>Add new course</Modal.Header>
-            <Modal.Content >
-              {<CreateCourse closeModal={this.closeCreateModal} addNewCourse={this.addNewCourse} />}
+            <Modal.Content>
+              {
+                <CreateCourse
+                  closeModal={this.closeCreateModal}
+                  addNewCourse={this.addNewCourse}
+                />
+              }
             </Modal.Content>
           </Modal>
 
-          <Modal size="fullscreen" open={this.state.addingHoles} onClose={this.close}>
+          <Modal
+            size='fullscreen'
+            open={this.state.addingHoles}
+            onClose={this.close}
+          >
             <Modal.Header>{this.state.courseName}</Modal.Header>
-            <Modal.Content >
-              {<EditCourse closeModal={this.closeHolesModal} courseId={this.state.courseId} courseName={this.state.courseName} />}
+            <Modal.Content>
+              {
+                <EditCourse
+                  closeModal={this.closeHolesModal}
+                  courseId={this.state.courseId}
+                  courseName={this.state.courseName}
+                />
+              }
             </Modal.Content>
           </Modal>
         </div>
@@ -138,4 +176,4 @@ class AdminCourses extends Component {
     }
   }
 }
-export default AdminCourses;
+export default AdminCourses
